b5a94949a4f1fff9ca9edd8b02d938495ad75824
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-08-05 08:17:09 +0700

    Adding raise Http404 and detail template

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.template import loader
 from models import Album
 
@@ -8,11 +8,15 @@ from models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = {
-        'all_albums' : all_albums
-    }
-    return render(request, 'music/index.html', context)
+    return render(request, 'music/index.html', {
+        'all_albums': all_albums
+    })
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for album id: " + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+
+    return render(request, 'music/detail.html', {'album': album})
