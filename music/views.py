83142d500b4db2ff7ff9a7b502db72c43ec16f3e
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-08-06 09:20:32 +0700

    Modifier models for favorite form

--- music/views.py
+++ music/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from models import Album
 
 # Create your views here.
@@ -14,9 +12,8 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+def favorite(request, album_id):
+    return "eiei"
