66a43a2713764efb6b1e1de51bef519b906088a8
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-08-07 21:45:41 +0700

    Adding Favorite form

--- music/views.py
+++ music/views.py
@@ -21,4 +21,12 @@ def favorite(request, album_id):
     try:
         selected_song = album.song_set.get(pk=request.POST['song'])
     except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message': "You did not selected a valid song"})
+    else:
+        if selected_song.is_favorite == True :
+            selected_song.is_favorite = False
+        else:
+            selected_song.is_favorite = True
+
+        selected_song.save()
         return render(request, 'music/detail.html', {'album': album})
