c42e0f85996898393b28c754dcc2c6eeac3c6ef8
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-08-09 00:20:49 +0700

    Adding Create Generic Views

---
+++ music/views_old.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404
+from models import Album
+
+# Create your views here.
+
+
+def index(request):
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {
+        'all_albums': all_albums
+    })
+
+
+def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except(KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {'album': album, 'error_message': "You did not selected a valid song"})
+    else:
+        if selected_song.is_favorite == True :
+            selected_song.is_favorite = False
+        else:
+            selected_song.is_favorite = True
+
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
