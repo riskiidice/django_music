5176466e43d568d84f961b55ddf52df065f132a1
Pattarawan Sriprasert <pattarawan@Macintosh.local>
2017-08-10 22:18:17 +0700

    Adding User Auth

--- music/models.py
+++ music/models.py
@@ -5,7 +5,7 @@ class Album(models.Model):
     artist = models.CharField(max_length=255)
     album_title = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
-    album_logo = models.FileField(upload_to='media')
+    album_logo = models.FileField(upload_to='album/%Y/%m/%d')
 
     def get_absolute_url(self):
         return reverse('music:detail', kwargs={'pk': self.pk})
--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     #/music/
     url(r'^$', views.IndexView.as_view(), name='index'),
 
+    #/register/
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     #/music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
